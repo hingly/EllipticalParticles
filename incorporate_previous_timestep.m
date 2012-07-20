d209abca04648168043fe4835cf0ce05a95aef7f
Helen Inglis <helen.inglis@up.ac.za>
2012-07-20 11:49:51 +0200

    lambda_max and loading variables implemented and tracked

--- incorporate_previous_timestep.m
+++ incorporate_previous_timestep.m
@@ -3,5 +3,6 @@ soln.Sigma_p(tt,:) = soln.Sigma_p(tt-1,:);
 soln.Eps_int(tt,:) = soln.Eps_int(tt-1,:);
 soln.sk(tt,:)=soln.sk(tt-1,:);
 
-stepload.lambda_max=loads.lambda_max(tt-1,:);
+stepcoh.lambda_max(:)=cohesive.lambda_max(tt-1,:);
+stepcoh.loading(:)=cohesive.loading(tt-1,:);
 stepload.MacroStrain(1)=loads.MacroStrain(tt,1);
