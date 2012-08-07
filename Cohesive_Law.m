a27a5defde8cc6d073fda3ab367e418e2ae1a1e7
Helen Inglis <helen.inglis@up.ac.za>
2012-08-07 14:48:46 +0200

    writing test for cohesive law

--- Cohesive_Law.m
+++ Cohesive_Law.m
@@ -30,10 +30,10 @@ lambda_e=material.lambda_e;
 zero_intpoints=zeros(1,NumPoints+1);
 
 % Previous maximum damage
-lambda_max=stepcoh.lambda_max;
+lambda_max=stepcoh.lambda_max
 
 % Are we loading or unloading?
-loading=stepcoh.loading;
+loading=stepcoh.loading
 
 % Initialise data
 stepcoh.traction=zero_intpoints;
@@ -61,11 +61,11 @@ tolerance=lambda_e/10;
 for jj=1:NumPoints+1
 
   % Displacement jump
-  U=real(displacement(jj));
-  V=imag(displacement(jj));
+  U=real(displacement(jj))
+  V=imag(displacement(jj))
   
   % compute damage parameter lambda
-  lambda(jj)=sqrt((U/delopen)^2+(V/delslide)^2);
+  lambda(jj)=sqrt((U/delopen)^2+(V/delslide)^2)
   
     
   %----------------------------------------------------
@@ -82,12 +82,12 @@ for jj=1:NumPoints+1
   % --- ktilde is the slope in the second stage
   % --- khat is the unloading slope
 
-  klinear=2*gint/lambda_e;                  
-  ktilde=2*gint/(1-lambda_e);
+  klinear=2*gint/lambda_e                  
+  ktilde=2*gint/(1-lambda_e)
   if lambda_max_temp(jj)>tolerance
-    khat=ktilde*(1-lambda_max_temp(jj))/lambda_max_temp(jj);
+    khat=ktilde*(1-lambda_max_temp(jj))/lambda_max_temp(jj)
   else
-    khat=klinear;
+    khat=klinear
   end
   
   
@@ -101,42 +101,50 @@ for jj=1:NumPoints+1
   % For nonlinear model
   if lambda(jj)<=lambda_e                   
     % Check for Stage I
-    
+    disp('Stage I')
     kn=klinear/delopen^2;
     kt=klinear/delslide^2;
     
   elseif lambda(jj)>lambda_e && lambda(jj)<1    
     % Check for Stage II
-    
+    disp('Stage II')
     if loading(jj)==1                        
       % Check for loading
+      disp('loading')
       kn=ktilde*(lambda(jj)-1)/lambda(jj)/delopen^2;
       kt=ktilde*(lambda(jj)-1)/lambda(jj)/delslide^2;
     
     else                                
       % unloading
+      disp('unloading')
       kn=khat/delopen^2;
       kt=khat/delslide^2;
     end
   
   elseif lambda(jj)>=1                      
     % Check for Stage III
+    disp('Stage III')
     kn=0;
     kt=0;
   else                                  
     % Check for errors
     error('Incorrect value of lambda',lambda(jj));   
   end
+
   
   % Now we correct for compression   **** Not sure we're doing this right!!!***
   % Allow damage in shear even under normal compression.  
-  if U<0
+  if U<0disp('U,V,lambda')
     kn=klinear/delopen^2;
   end
+  
+  kn 
+  kt
    
   % Find S,T interfacial tractions
-  S=kn*U;
-  T=kt*V;
+disp('S,T')
+  S=kn*U
+  T=kt*V
   
   stepcoh.traction(jj)=S+i*T;
   
@@ -158,4 +166,5 @@ stepcoh.loading_temp=loading_temp;
 stepcoh.lambda_max_temp=lambda_max_temp;
 stepcoh.lambda=lambda;
 
+pause
 %disp('Leaving Cohesive_Law...');
--- test_Cohesive_Law.m
+++ test_Cohesive_Law.m
@@ -3,11 +3,11 @@ function test_Cohesive_Law
 
 % FIXME : consider making these not hard-coded
 
-material.delopen=0.5
-material.delslide=0.5
-material.gint=5
+material.delopen=0.5;
+material.delslide=0.5;
+material.gint=5;
 %corresponds to sigmax=20
-material.lambda_e=0.001
+material.lambda_e=0.001;
 
 NumPoints=1;
 %Only 1 point around the ellipse
@@ -20,22 +20,65 @@ NumSteps=20;
 zero_intpoints=zeros(1,NumPoints+1);
 zero_stepsintpoints=zeros(NumSteps,NumPoints+1);
 
-stepdisp=zero_intpoints;
-
-stepcoh.traction=zero_intpoints;
-stepcoh.lambda=zero_intpoints;
-stepcoh.lambda_max=zero_intpoints;
-stepcoh.loading=zero_intpoints;
-stepcoh.lambda_max_temp=zero_intpoints;
-stepcoh.loading_temp=zero_intpoints;
 
 
 %--------------------------------------
-% Test Mode I tension loading
+% Test Mode I tension loading (modeI_tl)
 %======================================
+
+
+%Initialise structure for Mode I tension loading
+
+modeI_tl.displacement=zero_stepsintpoints;
+modeI_tl.cohesive.lambda=zero_stepsintpoints;
+modeI_tl.cohesive.lambda_max=zero_stepsintpoints;
+modeI_tl.cohesive.loading=zero_stepsintpoints;
+modeI_tl.cohesive.traction=zero_stepsintpoints;
+
+
+% Populate displacement vector
+u=linspace(0,material.delopen*1.1,NumSteps);
+v=zeros(NumSteps);
+
+for step=1:NumSteps
+  modeI_tl.displacement(step,1)=u(step)+i*v(step);
+  modeI_tl.displacement(step,2)= modeI_tl.displacement(step,1);
+end
+
+
 
 
 
 for step=1:NumSteps
+  
+  stepdisp=modeI_tl.displacement(step,:);
+  stepcoh.lambda=modeI_tl.cohesive.lambda(step,:);
+  stepcoh.lambda_max=modeI_tl.cohesive.lambda_max(step,:);
+  stepcoh.loading=modeI_tl.cohesive.loading(step,:);
+
+  
+  % Cohesive law
   stepcoh=Cohesive_Law(stepdisp,NumPoints,material,stepcoh);
+
+
+  % Write step quantites to stored quantities
+  modeI_tl.cohesive.lambda(step,:)=stepcoh.lambda;
+  modeI_tl.cohesive.traction(step,:)=stepcoh.traction;
+  modeI_tl.cohesive.lambda_max(step,:)=stepcoh.lambda_max_temp;
+  modeI_tl.cohesive.loading(step,:)=stepcoh.loading_temp;
+
+  % Prime stored quantities
+  if step<NumSteps
+    modeI_tl.cohesive.lambda_max(step+1,:)=modeI_tl.cohesive.lambda_max(step,:);
+    modeI_tl.cohesive.loading(step+1,:)=modeI_tl.cohesive.loading(step,:);
+  end
+
 end
+
+% $$$ modeI_tl.cohesive.traction(:,1)
+% $$$ modeI_tl.cohesive.lambda_max(:,1)
+% $$$ modeI_tl.cohesive.lambda(:,1)
+% $$$ modeI_tl.cohesive.loading(:,1)
+%plot(modeI_tl.displacement(:,1),modeI_tl.cohesive.traction(:,1),'bx-');
+
+
