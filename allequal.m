fdba9989261c99a1cac10cfbbdbdaa1d2d0f1027
Helen Inglis <helen.inglis@up.ac.za>
2012-09-25 20:40:36 +0200

    Modified allequal to take a mask

--- allequal.m
+++ allequal.m
@@ -1,6 +1,11 @@
-function a = allequal(b, c, epsilon)
+function a = allequal(b, c, epsilon, mask)
+
+if ~exist('mask', 'var')
+  mask = true(size(b));
+end
+
 if length(b) ~= length(c)
     error('nonconformant arguments');
 else
-    a = all(abs(b-c) < epsilon);
+    a = all(abs(b(mask)-c(mask)) < epsilon);
 end
