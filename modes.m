763c92e8c59264cb6ffcdd7a264c92e12a2aa6b3
Carl Sandrock <carl.sandrock@up.ac.za>
2013-05-08 10:25:03 +0200

    Vectorised modes.m

--- modes.m
+++ modes.m
@@ -14,25 +14,25 @@ phi=0;
 phiprime=0;
 psi=0;
 
+jj = -NumModes:2:-2;
+jindex = jj/2 + NumModes/2 + 1;
+phi_k(jindex) = R*forcecoeff(jindex).*((zeta.^(jj+1))./(jj+1) - m*(zeta.^(jj-1))./(jj-1));
+phiprime_k(jindex) = R*forcecoeff(jindex).*(zeta.^(jj) - m*zeta.^(jj-2));
+psi_k(jindex) = -R*forcecoeff(jindex).*(1 + m*zeta^2)./(zeta^2 - m).*(zeta.^(jj+1) - m*zeta.^(jj-1));
 
-for jj=-NumModes:2:NumModes
-    jindex=jj/2+NumModes/2+1;
-    if jj<=-2
-       phi_k(jindex)=R*forcecoeff(jindex)*((zeta^(jj+1))/(jj+1) - m*(zeta^(jj-1))/(jj-1));
-       phiprime_k(jindex)=R*forcecoeff(jindex)*(zeta^(jj) - m*zeta^(jj-2));
-       psi_k(jindex)=-R*forcecoeff(jindex)*(1+m*zeta^2)/(zeta^2-m)*(zeta^(jj+1) - m*zeta^(jj-1));
-    elseif jj==0
-       phi_k(jindex) = R*forcecoeff(jindex)*m/zeta;
-       phiprime_k(jindex) = -R*forcecoeff(jindex)*m/(zeta^2);
-       psi_k(jindex) = R*forcecoeff(jindex)*zeta*(1+m^2)/(zeta^2-m);
-    elseif jj>=2
-       phi_k(jindex)=0;
-       phiprime_k(jindex)=0; 
-       psi_k(jindex)=R*conj(forcecoeff(jindex))*((zeta^(-jj-1))/(jj+1)-m*(zeta^(-jj+1))/(jj-1));
-   end
-    phi=phi+phi_k(jindex);
-    phiprime=phiprime+phiprime_k(jindex);
-    psi=psi+psi_k(jindex);
-end
+jj = 0;
+jindex = jj/2 + NumModes/2 + 1;
+phi_k(jindex) = R*forcecoeff(jindex)*m/zeta;
+phiprime_k(jindex) = -R*forcecoeff(jindex)*m/(zeta^2);
+psi_k(jindex) = R*forcecoeff(jindex)*zeta*(1 + m^2)/(zeta^2 - m);
 
+jj = 2:2:NumModes;
+jindex = jj/2 + NumModes/2 + 1;
 
+phi_k(jindex) = 0;
+phiprime_k(jindex) = 0; 
+psi_k(jindex) = R*conj(forcecoeff(jindex)).*((zeta.^(-jj-1))./(jj+1)-m*(zeta.^(-jj+1))./(jj-1));
+
+phi = sum(phi_k);
+phiprime = sum(phiprime_k);
+psi = sum(psi_k);
