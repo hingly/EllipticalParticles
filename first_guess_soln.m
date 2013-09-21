1defdddc607881c67075ed874f9de4e9689bd070
Helen Inglis <helen.inglis@up.ac.za>
2013-09-21 09:36:31 +0200

    Reverted to symmetric first_guess for sigma_p_21

--- first_guess_soln.m
+++ first_guess_soln.m
@@ -21,7 +21,7 @@ if loads.SigBar_xy(1) < stress_epsilon
   soln.Sigma_p(tt,1) = 0;
   soln.Sigma_p(tt,2) = - loads.DriverStrain(tt)*material.E_m/((1+material.nu_m)*material.nu_m);
   soln.Sigma_p(tt,3) = soln.Sigma_p(tt,2)*loads.StressRatio_12_22;
-  soln.Sigma_p(tt,4) = 0; %soln.Sigma_p(tt,3);
+  soln.Sigma_p(tt,4) = soln.Sigma_p(tt,3);
 
   % Eps_int_11 is the same as the imposed macroscopic strain
   % Eps_int_22 is scaled by Poisson effect
@@ -40,7 +40,7 @@ else
   soln.Sigma_p(tt,1) = loads.DriverStrain(tt)*material.E_m;
   soln.Sigma_p(tt,2) = soln.Sigma_p(tt,1)*loads.StressRatio_22;
   soln.Sigma_p(tt,3) = soln.Sigma_p(tt,1)*loads.StressRatio_12;
-  soln.Sigma_p(tt,4) = 0; %soln.Sigma_p(tt,3);
+  soln.Sigma_p(tt,4) = soln.Sigma_p(tt,3);
 
   
   %First guess for soln.Eps_int
